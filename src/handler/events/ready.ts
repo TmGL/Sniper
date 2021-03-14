3eda343cea60a115a9143d8c6c36ee4d8b7e6a72
TmGL <alanharuwilliams@icloud.com>
2021-03-14 02:52:16 +0000

    feat(handler): set status

--- src/handler/events/ready.ts
+++ src/handler/events/ready.ts
@@ -1,11 +1,14 @@
 import { Event } from '../../interfaces';
 import { uri } from '../../config/dbconfig.json';
+import { activity } from '../../config/bot.json';
 import mongoose from 'mongoose';
 
 export const event: Event = {
     name: 'ready',
     run: client => {
         console.log(client.user.tag + ' is now ready!');
+
+        client.user.setActivity(activity.name, { type: 'LISTENING' });
 
         mongoose.connect(uri, {
             useNewUrlParser: true,
