89530248be400422155e2330a79367dcf6cd43a3
TmGL <alanharuwilliams@icloud.com>
2021-03-19 20:31:00 +0000

    refractor(client): destructor method from fs

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,7 +2,7 @@ import { Command, Event, Config } from '../interfaces';
 import { Client, Collection } from 'discord.js';
 import JsonConfig from '../config/bot.json';
 import path from 'path';
-import fs from 'fs';
+import { readdirSync } from 'fs';
 
 class Sniper extends Client {
     public commands: Collection<string, Command> = new Collection();
@@ -13,8 +13,8 @@ class Sniper extends Client {
         this.login(this.config.token);
 
         const commandPath = path.join(__dirname, "..", "handler", "commands");
-        fs.readdirSync(commandPath).forEach(dir => {
-            const commands = fs.readdirSync(`${commandPath}/${dir}`).filter(file => file.endsWith('.ts'));
+        readdirSync(commandPath).forEach(dir => {
+            const commands = readdirSync(`${commandPath}/${dir}`).filter(file => file.endsWith('.ts'));
             for (const file of commands) {
                 const { command } = require(`${commandPath}/${dir}/${file}`);
                 if (!command.name) {
@@ -30,7 +30,7 @@ class Sniper extends Client {
         });
 
         const eventPath = path.join(__dirname, "..", "handler", "events");
-        fs.readdirSync(eventPath).forEach(async file => {
+        readdirSync(eventPath).forEach(async file => {
             const { event } = await import(`${eventPath}/${file}`);
             this.events.set(event.name, event);
             this.on(event.name, event.run.bind(null, this));
